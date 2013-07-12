f7301726669887a8c5d0207800146c058cc4447b
Mygnu <harman@mygnu.org>
2013-07-12 12:45:10 +0930

    working with the tutorial

--- sq_root.py
+++ sq_root.py
@@ -1,11 +1,14 @@
-x = int(input('Enter an Integer: '))
 
-print(x,abs(x))
+
+x = int(input('Enter an Integer: ')) # prompts for Input
+
+
 ans = 0 # our guess starts at 0
 
 ##while the cube root of our guess is less than value of x
+
 while ans**3 < abs(x): 
-    ans +=1                                         #increases the guess by one 
+    ans +=1                         #increases the guess by one 
     print('current Guesses=',ans)
 if ans**3 != abs(x):
     print(x, 'is not a perfect cube')
@@ -13,3 +16,5 @@ else:
     if x <0:
         ans = -ans
     print('Cube root of', x,'is',ans)
+    
+

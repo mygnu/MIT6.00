b156e66c0015598953b1210e2a2dde7dd575f558
mygnu <harman@mygnu.org>
2013-07-22 14:44:36 +0930

    Little modifications

--- p-set1-a.py
+++ p-set1-a.py
@@ -1,10 +1,10 @@
 bal = 1000.00      #float(input('Balance on the credit Card:'))
-anint = 20      #float(input('Annual Interest Rate: '))
+anint = 10      #float(input('Annual Interest Rate: '))
 minp =3.0          #float(input('Minimum monthly repayment rate as percentage: '))
 tpaid=0
 
-##print('Balance=',bal,'\nInterest Rate=',anint,'%', '\
-##\nMinimum Monthley payment rate=',minp,'%\n\n')
+print('Balance=',bal,'\nInterest Rate=',anint,'%', '\
+\nMinimum Monthley payment rate=',minp,'%\n\n')
 
 for i in range(1,13,1):
     #i = i+1
@@ -12,13 +12,13 @@ for i in range(1,13,1):
     
     mp = (minp/100)* bal
     
-    print('monthly Repayment= $'+'{0:.2f}'.format(mp))
+    print('monthly Repayment= $'+'{0:.2f}'.format(mp)) # using a different function to round up
     
-    mint = (anint/12/100) * bal
-    print('Interest Paid= $'+str(round(mint,2)))
-    ppd = mp - mint
+    m_int = (anint/12/100) * bal
+    print('Interest Paid= $'+str(round(m_int,2)))
+    ppd = mp - m_int
     print('Principal Paid= $'+str(round(ppd,2)))
-    bal = bal-mp
+    bal = (bal-mp) + m_int
     print('Remaining Balance= $'+str(round(bal,2)))
     tpaid = mp+tpaid
    

a4e921c2ca6cf14d87c760c1d6d9e81047f60d51
Mygnu <harman@mygnu.org>
2013-07-24 16:08:40 +0930

    made a mistake when commiting

--- p-set1-b.py
+++ p-set1-b.py
@@ -1,24 +1,59 @@
 #!/usr/bin/env python3
 
-## This is my version of soluton with bisectional search 
+initialBalance = 1000 # input('Balance on the Credit Card:')
+interestRate = 15 # input('Interest Rate:')
 
 
-balance = 10000 #float(input('Balance on the credit Card:'))
-anint = 14 #float(input('Annual Interest Rate: '))
+monthlyPayment = 0
+monthlyInterest = interestRate/12/100
+balance = initialBalance
+numMonths = 0
 
-print('Balance= $'+str(balance),'\nInterest Rate= '+str(anint)+'%\n\n')
 
-minimum_pay = 1
-def payments(pay,bal):
-    for i in range(1,13):
-        monthly_int = anint/12
-        bal = bal * (1+monthly_int/100)- pay
-        #print('month', i,'balance =',round(bal,2))
-    return bal
+while balance>0:
+    monthlyPayment += 1
+    numMonths = 0
+    
+    
+    while numMonths<12 and balance > 0:
+        numMonths += 1
+        interest = monthlyInterest * balance
+        balance -= monthlyPayment
+        balance += interest
+balance = round(balance,2)
 
-while payments(minimum_pay,balance)> 0:
-    minimum_pay +=0.01
 
-rest =round((payments(minimum_pay,balance)),2)
-print('minimum monthly payment= $'+str(round(minimum_pay,2)),\
-'\nRemaining balance is= $'+str(rest))
+print('Monthly Payment=',monthlyPayment)
+print('Number of Months needed:', numMonths)
+print('Balance:', balance)
+
+
+
+
+
+
+
+
+
+
+
+## This is my version of 
+# balance = 10000 #float(input('Balance on the credit Card:'))
+# anint = 14 #float(input('Annual Interest Rate: '))
+
+# print('Balance= $'+str(balance),'\nInterest Rate= '+str(anint)+'%\n\n')
+
+# minimum_pay = 1
+# def payments(pay,bal):
+#     for i in range(1,13):
+#         monthly_int = anint/12
+#         bal = bal * (1+monthly_int/100)- pay
+#         #print('month', i,'balance =',round(bal,2))
+#     return bal
+
+# while payments(minimum_pay,balance)> 0:
+#     minimum_pay +=0.01
+
+# rest =round((payments(minimum_pay,balance)),2)
+# print('minimum monthly payment= $'+str(round(minimum_pay,2)),\
+# '\nRemaining balance is= $'+str(rest))
